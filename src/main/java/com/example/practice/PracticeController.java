7493b8feeaceb2d75c973589b9e35e818d70019b
gusk94 <h3652k@gmail.com>
2020-01-21 10:23:33 +0900

    Finish connect Database

--- src/main/java/com/example/practice/PracticeController.java
+++ src/main/java/com/example/practice/PracticeController.java
@@ -16,7 +16,7 @@ public class PracticeController {
     @RequestMapping("/index")
     private String jspTest() throws Exception{
         
-        System.out.println(mBoardMapper.boardCount());
+//        System.out.println(mBoardMapper.boardCount());
         
         return "index"; 
     }
